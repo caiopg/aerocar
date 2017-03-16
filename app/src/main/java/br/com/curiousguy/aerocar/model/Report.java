a65153cda7e6a3dbb608869ffaa23fb9d6114c99
Caio Gandra <caio.gandra@mandae.com.br>
2017-03-16 13:55:04 -0300

    Fixed lombok issue.

--- app/src/main/java/br/com/curiousguy/aerocar/model/Report.java
+++ app/src/main/java/br/com/curiousguy/aerocar/model/Report.java
@@ -7,7 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
+@AllArgsConstructor(suppressConstructorProperties = true)
 public class Report {
 
     private Date createdIn;
